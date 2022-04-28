57998af9b170217b0520ea64e3ad8f744b1c4bb6
pks9 <pks9@illinois.edu>
2022-04-28 04:25:12 -0500

    much much better audio processing. We now get clean frequencies.

--- sound stuff.py
+++ sound stuff.py
@@ -2,7 +2,10 @@
 import pyaudio
 import struct
 import numpy as np
-from scipy.fftpack import fft
+from matplotlib import pyplot as plt
+from tkinter import TclError
+from scipy.fft import fft
+from scipy.fft import fftfreq
 
 
 # constants
@@ -25,15 +28,26 @@ stream = p.open(
 )
 # an array containing possible frequencies that we are considering from the input
 freq_range = np.linspace(0, RATE, CHUNK)
+freq_range_nonv = freq_range.reshape(len(freq_range), 1)
+fig, ax = plt.subplots()
+x = np.arange(0, 2*CHUNK, 2)
+line, = ax.semilogx(freq_range, np.random.rand(CHUNK))
+ax.set_ylim(0, 1)
+ax.set_xlim(20, RATE/2)
+fig.show()
+# print("freq start")
 
 while True:
     # reading from the stream
     data = stream.read(CHUNK)
-    data_int = np.array(struct.unpack(str(2*CHUNK) + 'B', data), dtype='b')[::2]+127
-    data_fft = fft(data_int)
-    # we find the index of the max value from the data we read and find the frequency at that index from the frequency array
-    max_val = np.max(data_fft)
-    freq_index = np.where(data_fft == max_val)
-    print(freq_range[freq_index])
-
+    #data_int = np.array(struct.unpack(str(2*CHUNK) + 'B', data), dtype='b')[::2]+127
+    data_int = struct.unpack(str(CHUNK) + 'h', data)
+    data_fft = np.abs(fft(data_int))*2/(33000*CHUNK)
+    data_fft_nonv = data_fft.reshape(len(data_fft), 1)
+    max = np.amax(data_fft_nonv)
+    ind = np.where(data_fft==max)
+    print(freq_range[ind][0])
+    line.set_ydata(data_fft)
+    fig.canvas.draw()
+    fig.canvas.flush_events()
 # made with ❤ for project A.V.A.J
